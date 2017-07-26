19c7eff38de7938bbe26ccdecdca9fbcd0d76de0
Nestor <nestoralvaro@gmail.com>
2017-07-26 23:48:42 +0200

    Adding init file to use the code as a module

---
+++ pos/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from lemmatizer.config import LEMMATIZATION_FILE_PATH
+from lemmatizer.lemmatizer import Spanish_Lemmatizer
+
+__version__ = '0.0.1'
+__all__ = ['LEMMATIZATION_FILE_PATH', 'Spanish_Lemmatizer']
