b90c1b62027d247156e6cbebf901c85c7ffcc81e
Nestor <nestoralvaro@gmail.com>
2017-07-28 12:28:48 +0200

    Update __init__.py

--- pos/__init__.py
+++ pos/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-from lemmatizer.config import LEMMATIZATION_FILE_PATH
-from lemmatizer.lemmatizer import Spanish_Lemmatizer
 
-__version__ = '0.0.1'
-__all__ = ['LEMMATIZATION_FILE_PATH', 'Spanish_Lemmatizer']
